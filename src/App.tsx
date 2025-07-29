bbf115b046bfa64c77b7b0667430a6c63d0cea6c
Marcelo <yepemar99@gmail.com>
2025-07-29 21:42:08 +0200

    install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h2 className="text-center">Hello world</h2>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button>Button</Button>
+      </div>
     </>
   );
 }
